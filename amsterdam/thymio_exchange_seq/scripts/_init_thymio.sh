0e34ac8c27ee9268c70ab5e222c06e731150d358
RPI <asriellursat@gmail.com>
2015-08-14 16:48:38 +0200

    conflicts

--- amsterdam/thymio_exchange_seq/scripts/_init_thymio.sh
+++ amsterdam/thymio_exchange_seq/scripts/_init_thymio.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+hostIP=$1
+
 sleep 5
 (asebamedulla "ser:device=/dev/ttyACM0" &)
 sleep 1
-python /media/Data/GoogleDrive/devices/master_project/thymio_exchange_seq/src/exchange_seq.py -d
+
+if [ -z $hostIP ]
+then
+	python /home/pi/dev/thymioPYPI/amsterdam/thymio_exchange_seq/src/exchange_seq.py -d
+else
+	python /home/pi/dev/thymioPYPI/amsterdam/thymio_exchange_seq/src/exchange_seq.py -d -i $hostIP
+fi
