ca28565e20f8741d9776644a6ceb98ff7602656e
CAThanatos <asriellursat@gmail.com>
2015-10-08 22:14:39 +0200

    misc + CollectiveGatheringPlus WIP

--- OctoPY/rpifiles/SimulationBraitenberg/SimulationBraitenberg.py
+++ OctoPY/rpifiles/SimulationBraitenberg/SimulationBraitenberg.py
@@ -8,6 +8,11 @@ class SimulationBraitenberg(Simulation.Simulation) :
 	def __init__(self, controller, mainLogger) :
 		Simulation.Simulation.__init__(self, controller, mainLogger)
 
+		if Params.params.avoidance == "False" :
+			self.__avoidance = False
+		else :
+			self.__avoidance = True
+
 	def preActions(self) :
 		pass
 
@@ -17,7 +22,7 @@ class SimulationBraitenberg(Simulation.Simulation) :
 		self.waitForControllerResponse()
 		self.tController.writeMotorsSpeedRequest([0, 0])
 
-	def Braitenberg(self, proxSensors, avoidance = False) :
+	def Braitenberg(self, proxSensors, avoidance) :
 		if not avoidance :
 			leftWheel = [-0.1, -0.05, -0.001, 0.06, 0.15]
 			rightWheel = [0.12, 0.07, -0.002, -0.055, -0.11]
@@ -33,8 +38,12 @@ class SimulationBraitenberg(Simulation.Simulation) :
 			totalRight = totalRight + (proxSensors[i] * rightWheel[i])
 
 		# Add a constant speed
-		totalRight = totalRight + 200
-		totalLeft = totalLeft + 200
+		if not avoidance :
+			totalRight = totalRight + 150
+			totalLeft = totalLeft + 150
+		else :
+			totalRight = totalRight + 200
+			totalLeft = totalLeft + 200
 
 		self.tController.writeMotorsSpeedRequest([totalLeft, totalRight])
 
@@ -48,7 +57,7 @@ class SimulationBraitenberg(Simulation.Simulation) :
 			self.waitForControllerResponse()
 			PSValues = self.tController.getPSValues()
 
-			self.Braitenberg(PSValues, bool(Params.params.avoidance))			
+			self.Braitenberg(PSValues, self.__avoidance)			
 		except :
 			self.mainLogger.critical('SimulationDefault - Unexpected error : ' + str(sys.exc_info()[0]) + ' - ' + traceback.format_exc())
 
