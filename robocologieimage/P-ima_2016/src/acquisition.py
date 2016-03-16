748d65fa325c59186da9940347596d8d5ba1ed16
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2016-03-17 00:25:20 +0100

    Add P-ima_2016 (Multiple Camera Tag Detection)

---
+++ robocologieimage/P-ima_2016/src/acquisition.py
@@ -0,0 +1,110 @@
+ #!/usr/bin/python
+ # -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+
+class Material(object):
+    def __init__(self, id_):
+        """ id_ - Filepath or Camera Index """
+        self.id = id_
+        self.frame = None
+        self.results = {}
+        self.done = False
+
+    def next(self):
+        ret, frame = self.device.read()
+        if (ret == False): # failed to capture
+            if not self.done:
+                print "Camera ended (id={})".format(self.id)
+            done = True
+            return done, np.zeros((360,640, 3), np.uint8)
+        #frame = cv2.resize(frame, (640*2,360*2), interpolation=cv2.INTER_AREA)
+        done = False
+        return done, frame
+
+    def update(self):
+        self.done, self.frame = self.next()
+
+    def kill(self):
+        """ Releases video capture device. """
+        cv2.VideoCapture(self.id).release()
+
+class Video(Material):
+    def __init__(self, video_id):
+        super(Video, self).__init__(video_id)
+        self.device = cv2.VideoCapture(self.id)
+        self.active = True
+
+class Camera(Material):
+    def __init__(self, cam_id):
+        super(Camera, self).__init__(cam_id)
+        self.active = self.open()
+
+    def open(self):
+        try:
+            self.device = cv2.VideoCapture(self.id)
+        except cv2.error as e:
+            print "Camera not connected (id={})".format(self.id)
+            return False
+        else:
+            print "New camera added (id={})".format(self.id)
+            self.device.set(cv2.CAP_PROP_FRAME_HEIGHT, 600*2);
+            self.device.set(cv2.CAP_PROP_FRAME_WIDTH, 800*2);
+            self.device.set(cv2.CAP_PROP_FPS, 30);
+            print "Parameters: {}x{} fps={}".format(int(self.device.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.device.get(cv2.CAP_PROP_FRAME_HEIGHT)), self.device.get(cv2.CAP_PROP_FPS))
+        return True
+
+    def update(self):
+        super(Camera, self).update()
+
+class Controller(object):
+    def __init__(self, ids, mode):
+        assert isinstance(ids, tuple)
+        if mode == 'CAMERA':
+            self.cameras = {camid : Camera(camid) for camid in ids}
+        elif mode == 'VIDEO':
+            self.cameras = {camid : Video(camid) for camid in ids}
+
+    def kill(self, ids):
+        """
+        Kill cameras whose identifier is found in `camera_list`.
+        camera_list : camera object list
+        """
+        for cam_id in ids:
+            self.cameras[cam_id].kill()
+
+    def addCamera(self, cam_id):
+        if cam_id in self.cameras.keys():
+            return False
+        self.cameras[cam_id] = Camera(cam_id)
+        return True
+
+    def openCamera(self, cam_id):
+        assert cam_id in self.cameras.keys()
+        camera = self.cameras[cam_id]
+        if not camera.active:
+            camera.active = camera.open()
+        return camera.active
+
+    def getAll(self):
+        return {cam_id : cam for cam_id, cam in self.cameras.items()}
+
+    def killAll(self):
+        self.kill(self.cameras.keys())
+
+    def getActive(self):
+        return {cam_id: cam for cam_id, cam in self.cameras.items() if not cam.done and cam.active}
+
+    def update(self, new_cam_ids=[]):
+        for cam_id in new_cam_ids:
+            exist = self.addCamera(cam_id)
+            if not exist:
+                self.openCamera(cam_id)
+
+        live_cams = self.getActive()
+        for cam_id, cam in live_cams.items():
+            cam.update()
+
+    @property
+    def active(self):
+        return len(self.getActive().values()) != 0
---
+++ robocologieimage/P-ima_2016/src/interface.py
@@ -0,0 +1,161 @@
+from PIL import Image, ImageTk
+import Tkinter as tk
+import time
+import cv2
+import numpy as np
+
+class Window:
+    def switch_mode(self, mode):
+        self.mode = mode
+
+    def close_windows(self):
+        self.master.quit()
+        self.master.destroy()
+        self.alive = False
+
+    def update_image(self, frame, channel, h, w):
+        frame = cv2.resize(frame, (h, w), interpolation=cv2.INTER_NEAREST)
+        img = Image.fromarray(frame)
+        imgtk = ImageTk.PhotoImage(image=img)
+        channel.imgtk = imgtk
+        channel.configure(image=imgtk)
+
+class MainWindow(Window):
+    def __init__(self, master, title):
+        self.title = title
+        self.fps = 0
+        self.fps_mean = 0
+        self.framerate = None # label
+        self.channels = {}
+        self.tag_channels = {}
+        self.mode = 'markers'
+        self.marker_app = None
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.adv_button = tk.Button(self.frame, text = 'New Window', width = 25, command = self.adv_window)
+        self.setup()
+        #self.adv_button.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+        self.frame.pack(fill=tk.BOTH, expand=tk.YES)
+
+    def setup(self):
+        self.main_group = tk.LabelFrame(self.frame)
+        self.main_group.pack(fill=tk.BOTH, expand=1)
+        #cameras
+        self.camera_group = tk.LabelFrame(self.main_group)
+        self.camera_group.pack(side=tk.LEFT,anchor=tk.NW)
+        buttons_frame = tk.LabelFrame(self.camera_group)
+        infos = [("original", 1), ("canny", 1), ("edges", 1), ("markers", 1), ("path", 1)]
+        for mode, height in infos:
+            tk.Button(buttons_frame, text=mode.capitalize(), command=lambda mode=mode: self.switch_mode(mode), height=height).pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        buttons_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.channel_group = tk.LabelFrame(self.camera_group)
+        self.channel_group.pack(side=tk.LEFT)
+        #markers
+        self.tag_channel_group = tk.LabelFrame(self.main_group)
+        self.tag_channel_group.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        #others
+        self.information_bar = tk.LabelFrame(self.frame)
+        self.information_bar.pack(fill=tk.X, expand=0)
+        self.info_txt = tk.Label(self.information_bar, text='')
+        self.info_txt.pack(fill=tk.X, expand=0)
+
+    def create_channel(self, cam_id):
+        r = len(self.channels) // 2
+        c = len(self.channels) % 2
+        self.channels[cam_id] = {}
+        self.channels[cam_id]['group'] = tk.LabelFrame(self.channel_group)
+        self.channels[cam_id]['group'].grid(row=r, column=c)
+        self.channels[cam_id]['image'] = tk.Label(self.channels[cam_id]['group'])
+        self.channels[cam_id]['image'].pack(side=tk.TOP)
+        self.channels[cam_id]['descr'] = tk.Label(self.channels[cam_id]['group'], text = '', width = 25)
+        self.channels[cam_id]['descr'].pack(fill=tk.X, expand=1)
+
+    def create_tag_channel(self, marker_id):
+        r = len(self.tag_channels) // 4
+        c = len(self.tag_channels) % 4
+        self.tag_channels[marker_id] = {}
+        self.tag_channels[marker_id]['group'] = tk.LabelFrame(self.tag_channel_group)
+        self.tag_channels[marker_id]['group'].grid(row=r, column=c, sticky='wens')
+        self.tag_channels[marker_id]['image'] = tk.Label(self.tag_channels[marker_id]['group'])
+        self.tag_channels[marker_id]['image'].pack(side=tk.LEFT)
+        self.tag_channels[marker_id]['ref_img'] = tk.Label(self.tag_channels[marker_id]['group'])
+        self.tag_channels[marker_id]['ref_img'].pack(side=tk.LEFT)
+        self.tag_channels[marker_id]['descr'] = tk.Label(self.tag_channels[marker_id]['group'], text = '')
+        self.tag_channels[marker_id]['descr'].pack(fill=tk.X, expand=1, side=tk.LEFT)
+        self.tag_channel_group.columnconfigure(c, weight=1)
+        self.tag_channel_group.rowconfigure(r, weight=1)
+
+    def update_fps(self, seconds):
+        # Calculate frames per second
+        self.fps  = 1 / seconds
+        self.fps_mean = self.fps_mean + (self.fps - self.fps_mean)
+        self.master.title(self.title + ' - FPS: {} (avg. {})'.format(round(self.fps, 1), round(self.fps_mean, 1)))
+
+    def update_markers(self, detectors, seconds):
+        for cam_id, detector in detectors.items():
+            for marker_id, marker in detector.markers_dict.items():
+                if not marker_id in self.tag_channels.keys() and detector.detect_time[marker_id] > 0.5:
+                    self.create_tag_channel(marker_id)
+                if detector.detect_time[marker_id] > 0.5:
+                    self.tag_channels[marker_id]['descr']['text'] = "Tag {}\n{},{}\n{}sec".format(str(marker_id), detector.positions[marker_id][0][0], detector.positions[marker_id][0][1], round(detector.detect_time[marker_id], 1))
+                    # Update images
+                    frame = detector.homothetie_markers[marker_id]
+                    self.update_image(frame, self.tag_channels[marker_id]['image'], 20, 20)
+                    # Update ref_images
+                    frame = detector.refs[0][marker_id]*255
+                    self.update_image(frame, self.tag_channels[marker_id]['ref_img'], 20, 20)
+
+    def update_cameras(self, cameras, detectors, seconds):
+        for cam_id, cam in cameras.items():
+            detector = detectors[cam_id]
+            if not cam_id in self.channels.keys():
+                self.create_channel(cam_id)
+            # Update texts
+            self.channels[cam_id]['descr']['text'] = 'cam {} {} {} tags {}'.format(str(cam_id)[-8:-1], " "*10, detector.curr_mk, detector.detected)
+            # Update images
+            frame = detectors[cam_id].get(self.mode).copy()
+            w, h = frame.shape[:2]
+            w, h = max(w-w*70/100, 200), max(h-h*70/100, 300)
+            self.update_image(frame, self.channels[cam_id]['image'], h, w)
+
+    def update(self, cameras, detectors, seconds):
+        self.update_fps(seconds)
+        self.update_markers(detectors, seconds)
+        self.update_cameras(cameras, detectors, seconds)
+        self.info_txt['text'] = "{} active, {} (mode {})".format(len(cameras), len(set([marker_id for detector in detectors.values() for marker_id in detector.markers_dict.keys()])), self.mode)
+
+    def adv_window(self):
+        self.advWindow = tk.Toplevel(self.master)
+        self.marker_app = AdvancedWindow(self.advWindow, self)
+
+class AdvancedWindow(Window):
+    def __init__(self, master, main):
+        self.main = main
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.setup()
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+        self.alive = True
+        self.mode = self.main.mode
+        self.channels = {}
+
+class Interface(object):
+    def __init__(self, title):
+        self.root = tk.Tk()
+        self.root.title(title)
+        self.app = MainWindow(self.root, title)
+
+    def update(self, cameras, detectors, seconds):
+        self.app.update(cameras, detectors, seconds)
+        self.root.update()
+        self.root.update_idletasks()
+        cv2.waitKey(1)
+
+    @property
+    def exit(self):
+        return cv2.waitKey(1) >= 0
+
+    def kill(self):
+        cv2.destroyAllWindows()
