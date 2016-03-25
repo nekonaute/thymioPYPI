17e045d44cbdcebb2fc0c4625549f4e6f4dbed91
ropaul <yannteddy.ropaul@gmail.com>
2016-03-25 18:17:56 +0100

    update vanillaEE

--- OctoPY/Backup/three_fold_adaptation/scripts/copy_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read line
-do
-	echo "COPY '$1' TO $line"
-	scp ../rpis/"$1" pi@"$line":/home/pi/rpis/$1 &
-	echo -e "\r"
-done <./bots.txt
-wait
--- README.md
+++
@@ -1,3 +0,0 @@
-# thymioPYPI
-
-Work in progress : framework for programming a Thymio2 with a RaspberryPI, with camera, and with Python 2.X
