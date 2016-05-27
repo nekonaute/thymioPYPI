6a12b7c7a618af7ea558bd1d4eee9260020c43b6
ropaul <yannteddy.ropaul@gmail.com>
2016-05-27 16:34:26 +0200

    commucation vanilla EE

--- OctoPY/rpifiles/VanillaEE/SimulationVanillaEE.py
+++ OctoPY/rpifiles/VanillaEE/SimulationVanillaEE.py
@@ -14,7 +14,7 @@ import genome
 import vision
 import random
 import threading
-
+import traceback
 
 import time
 
@@ -31,12 +31,12 @@ class SimulationVanillaEE(Simulation.Simulation) :
 	def __init__(self, controller, mainLogger) :
 		
 		#Simulation.Simulation.__init__(self, controller, mainLogger)
-		self.myGenome = genome.genome(7*2*3+2)
+		self.myGenome = genome.genome(7*2*3+2)# 7 sensor * 2 output * 3 type of thing to detect + 1 "control" value per output
 		self.eye = vision.vision() #class with the technik of captation of the environnement
 		self.resources = RESOURCES; # find a way to initiate all of that
 		self.fitness =0;
 		self.fitnessWindow = []
-		self.fitnessWindowSize = 50
+		self.fitnessWindowSize = 10
 		self.genomePool= [] #stock the genome of the other robots
 		self.fitnessPool= [] #stock the fitness of the other robots
 		self.alive = True
@@ -44,6 +44,17 @@ class SimulationVanillaEE(Simulation.Simulation) :
 		self.ticks = 0
 		self.generations = 400
 		
+		
+		"""communication"""
+		listRpis = Params.params.list_rpis
+		self.__listRpis = listRpis.split(',')
+		self.__frequencyMess = int(Params.params.frequency_mess)
+		self.__maxMess = int(Params.params.max_mess)
+		self.__listMessages = list()
+		
+		""" acceleration"""
+		self.oldtransitifAcelleration= 0
+		self.oldAngularAcelleration= 0
 
 	def preActions(self) :
 		pass
@@ -56,12 +67,17 @@ class SimulationVanillaEE(Simulation.Simulation) :
 			self.ticks +=1
 			
 			#if (self.alive):
-				
-			self.evaluation()
+			if( self.alive):
+					
+				self.evaluation()
+			
+				self.updateFitness()
+			
+				print self.fitness
 		
-			self.resourcesManagement()
+# 			self.resourcesManagement()
 		
-			self.tchatBetweenRobot()
+# 			self.tchatBetweenRobot()
 		
 			self.evolution()
 			
@@ -78,7 +94,7 @@ class SimulationVanillaEE(Simulation.Simulation) :
 		s = ""
 		for i in range (len(entry)):
 			s+= str(entry[i]) + "|"
-		print s
+# 		print s####################################################################################
 		left = self.myGenome.evaluationLeft(entry) 
 		right = self.myGenome.evaluationRight(entry) 
 		print "left=" +str(left) + "  right = " + str(right)
@@ -93,8 +109,8 @@ class SimulationVanillaEE(Simulation.Simulation) :
 			self.alive = False
 	
 	def tchatBetweenRobot(self):
-		#
-		pass
+		self.broadcast()
+		
 	
 	def evolution(self):
 		if (self.ticks == self.generations):
@@ -110,12 +126,15 @@ class SimulationVanillaEE(Simulation.Simulation) :
 					self.resources =0
 					self.fitness = 0
 					self.resourcesManagement()
-					return 
-				
-				self.myGenome = child.MutationGaussian(self.sigma)
+					self.alive = 0
+					
+					 
+				else
+					self.myGenome = child.MutationGaussian(self.sigma)
+					self.resources = RESOURCES
+					self.alive = 1
 				self.genomePool = []
 				self.fitnessPool = []
-				self.resources = RESOURCES 
 				self.fitness = 0
 		
 		
@@ -144,7 +163,57 @@ class SimulationVanillaEE(Simulation.Simulation) :
 		return  self.myGenome.equals (other.myGenome)
 			
 		
-	
+	def receiveComMessage(self, data) :
+		sender = ""
+		value = []
+		if "senderHostname" in data.keys() :
+			sender = data["senderHostname"]
+
+			if "value" in data.keys() :
+				value = data["value"]
+				self.__listMessages.append((sender, value))
+				self.genomePool.append(value[0])
+				self.fitnessPool.append(value[1])
+			else :
+				self.mainLogger.error('SimulationTestCommunication - Receiving message from ' + str(sender) + ' without value data : ' + str(data))
+		else :
+			self.mainLogger.error('SimulationTestCommunication - Receiving message without sender : ' + str(data))
+		
+		"""FAIRE L'ENREGISTREMENT"""
+		while len(self.__listMessages) > 0 :
+				message = self.__listMessages.pop(0)
+				self.mainLogger.debug('SimulationTestCommunication - Received ' + str(message[1]) + ' from ' + str(message[0]))
+
+
+
+
+	def broadcast (self):
+		try :
+			
+			if (self.__cptStep % self.__frequencyMess == 0) and ((self.__cptStep/self.__frequencyMess) < self.__maxMess) :
+				recipientsList = ''
+
+				for rpi in self.__listRpis :
+					recipientsList += str(rpi) + ','
+				self.sendMessage(recipients = recipientsList, value = (self.myGenome, self.fitness))
+		except :
+			self.mainLogger.critical('SimulationTestCommunication - Unexpected error' )
+
+
+	def updateFitness(self) :
+		Sk = 0
+		for i in range (7):
+			Sk = max (getSensorValue(i))
+		getSensorValue(i)
+		self.fitness = self.getTrasitiveAcceleration() * ( 1 - self.getAngularAceeleration() * (1- Sk/DISTANCE_MAX_VISION ))
+		self.oldtransitifAcelleration = getAccelerometerValue(0)
+		self.oldAngularAcelleration= getAccelerometerValue(1)
+		
+	def getTrasitiveAcceleration(self):
+		return getAccelerometerValue(0) - self.oldtransitifAcelleration
+		
+	def getAngularAceeleration(self):
+		return getAccelerometerValue(1) - self.oldAngularAcelleration	
 		
 """		
 main = SimulationVanillaEE (None, None)
